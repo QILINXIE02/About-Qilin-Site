7955d519a91cfc070dc7a62443b89ae707b5a674
Qilin Xie <qxie@gicw.org>
2023-10-25 13:37:27 -0700

    fixing buggs

--- ignoreplz.js
+++ ignoreplz.js
@@ -1,22 +1,69 @@
-'use strict';
+// for lab 3 only
 
-// Track the number of correct answers
-let correctAnswers = 0;
+// User's 6th question for guessing a number
+function guessNumber() {
+    let correctNumber = 7; // Change this to your desired number
+    for (let i = 0; i < 4; i++) {
+        let userGuess = parseInt(prompt("Guess a number (1-10):"));
+        if (userGuess === correctNumber) {
+            alert("Correct! You guessed the right number.");
+            console.log("User guessed the correct number.");
+            break;
+        } else if (userGuess < correctNumber) {
+            alert("Too low. Try again.");
+            console.log("User guessed too low.");
+        } else if (userGuess > correctNumber) {
+            alert("Too high. Try again.");
+            console.log("User guessed too high.");
+        }
+    }
+    alert("The correct number was 7.");
+    console.log("User didn't guess the correct number.");
+}
+
+// Call the new function when you reach this point in your script
+guessNumber();
+
+
+// User's 7th question with multiple possible answers
+function guessMultipleAnswers() {
+    const possibleAnswers = ["apple", "banana", "cherry", "date"];
+    for (let i = 0; i < 6; i++) {
+        let userAnswer = prompt("Guess a fruit:");
+        if (possibleAnswers.includes(userAnswer.toLowerCase())) {
+            alert("Correct! That's one of the possible answers.");
+            console.log("User guessed a correct answer.");
+            break;
+        } else {
+            alert("Try again.");
+            console.log("User guessed an incorrect answer.");
+        }
+    }
+    alert("Possible answers were: " + possibleAnswers.join(", "));
+    console.log("User didn't guess a correct answer.");
+}
+
+// Call the new function when you reach this point in your script
+guessMultipleAnswers();
+
+
+
+//1:17pm copy
+'use strict';
 
 // Function to ask a yes/no question and then check the answer
 function askQuestion(question, correctAnswer) {
     const userAnswer = prompt(question);
-
+    
     if (userAnswer === null) {
         alert("Game Over. You canceled the game.");
-        return;
+    return;
     }
 
     const userAnswerNormalized = userAnswer.toLowerCase().trim();
     if (userAnswerNormalized === correctAnswer) {
         alert("Correct!");
         console.log(`User answered question correctly: ${question}`);
-        correctAnswers++; // Increment the correct answers count
     } else {
         alert("Incorrect.");
         console.log(`User answered question incorrectly: ${question}`);
@@ -50,6 +97,7 @@ if (userName) {
     alert("Game Over. You canceled the game.");
 }
 
+
 // User's 6th question for guessing a number
 function guessNumber() {
     let correctNumber = 7; // Change this to your desired number
@@ -58,7 +106,6 @@ function guessNumber() {
         if (userGuess === correctNumber) {
             alert("Correct! You guessed the right number.");
             console.log("User guessed the correct number.");
-            correctAnswers++; // Increment the correct answers count
             break;
         } else if (userGuess < correctNumber) {
             alert("Too low. Try again.");
@@ -75,15 +122,15 @@ function guessNumber() {
 // Call the new function when you reach this point in your script
 guessNumber();
 
+
 // User's 7th question with multiple possible answers
 function guessMultipleAnswers() {
-    const possibleAnswers = ["apples", "bananas", "blackberries", "tomatoes"];
+    const possibleAnswers = ["apple", "banana", "blackberries", "tomatoes"];
     for (let i = 0; i < 6; i++) {
         let userAnswer = prompt("Guess my favorite fruits/veggies (lower case & answer in plural only please):");
         if (possibleAnswers.includes(userAnswer.toLowerCase())) {
             alert("Correct! That's one of the possible answers.");
             console.log("User guessed a correct answer.");
-            correctAnswers++; // Increment the correct answers count
             break;
         } else {
             alert("Try again.");
@@ -97,9 +144,13 @@ function guessMultipleAnswers() {
 // Call the new function when you reach this point in your script
 guessMultipleAnswers();
 
+
+// Track the number of correct answers
+let correctAnswers = 0;
+
 // Display the final score
 alert(`You got ${correctAnswers} out of 7 questions correct.`);
 console.log(`User's final score: ${correctAnswers}/7`);
 
-// Thank the user and provide a message
-alert(`Thanks for participating, ${userName}, now take a look at my website if you wish! ~~ Have a good day! ~`);
+
+
